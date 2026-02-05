9f5fc6ee8f5dc80646eb77dfdd5bdac419521844
isaackhlam <97761927+isaackhlam@users.noreply.github.com>
2026-02-05 22:14:58 +0800

    feat: wrap get user selection as promise

--- src/game/actions/getNoble.ts
+++ src/game/actions/getNoble.ts
@@ -2,28 +2,23 @@ import {canGetNoble} from "../rules/canGetNoble";
 import {Noble, Player} from "../types";
 
 
-export const getNoble = (
+export const getNoble = async (
   player: Player,
   nobles: (Noble | null)[],
+  chooseIndex: (options: number[]) => Promise<number>,
 ) => {
-  const gettableNoble = nobles.map(() => false);
-  for(let i = 0; i < nobles.length; i++){
-    const currentNoble = nobles[i];
-    if(currentNoble && canGetNoble(player, currentNoble)) {
-      gettableNoble[i] = true;
-    }
-  }
-  const numOfgetbaleNoble = gettableNoble.reduce((sum: number, x: boolean) => sum + (x? 1: 0), 0);
-  if(numOfgetbaleNoble === 0) return;
-  let idx;
-  if(numOfgetbaleNoble === 1) {
-    idx = gettableNoble.indexOf(true);
-  }else{
-    //!! TODO: Ask selection of noble if multiple noble could be get.
-    idx = 0; // Placeholder now.
-  }
+  const options = nobles
+    .map((n, i) => (n && canGetNoble(player, n) ? i : -1))
+    .filter(i => i !== -1);
+
+  if (options.length === 0) return;
+
+  const idx =
+    options.length === 1
+      ? options[0]
+      : await chooseIndex(options);
+
   const noble = nobles[idx]!;
   player.nobles.push(noble);
   nobles[idx] = null;
-
 }
